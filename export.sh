e353b11a080aa6fefc7c89a94bc2aeca08571c9e
Lewis Wood <lewislwood@gmail.com>
2022-02-12 11:51:40 -0600

    export.sh

--- export.sh
+++ export.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
-_os="`uname`"
+_os="`uname`" 
 _now=$(date +"%m_%d_%Y")
 _file="wp-data/data_$_now.sql"
 
 # Export dump
-
-docker-compose exec db sh -c 'exec mysqldump "wp" -uroot -p"$MYSQL_ROOT_PASSWORD      "' > $_file
+EXPORT_COMMAND='exec mysqldump "$MYSQL_DATABASE" -uroot -p"$MYSQL_ROOT_PASSWORD"'
+docker-compose exec db sh -c "$EXPORT_COMMAND" > $_file
 
 if [[ $_os == "Darwin"* ]] ; then
   sed -i '.bak' 1,1d $_file
 else
   sed -i 1,1d $_file # Removes the password warning from the file
 fi
-
-
-#       $MYSQL_DATABASE
-
